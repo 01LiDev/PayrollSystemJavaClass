627f3c4377f0eaaeb2825b9f830c4d80c15fe5fb
Johnson Li <liwindx15@gmail.com>
2023-04-25 00:27:33 -0400

    Finished Payroll System Modification

---
+++ src/main/java/com/jldevelops/payrollsystem/PayrollSystem.java
@@ -0,0 +1,35 @@
+package com.jldevelops.payrollsystem;
+
+
+public class PayrollSystem {
+
+    public static void main(String[] args) {
+        // Get the current month
+        int currentMonth = 3;
+
+        // Create an array of Employee objects
+        Employee[] employees = new Employee[4];
+
+        // Add sample employees to the array
+        employees[0] = new SalariedEmployee("John", "Doe", "123-45-6789", 800.0, new Date(3, 15, 1990));
+        employees[1] = new HourlyEmployee("Jane", "Smith", "234-56-7890", 20.0, 40, new Date(3, 10, 1985));
+        employees[2] = new CommissionEmployee("Alice", "Brown", "345-67-8901", 0.15, 0.06, new Date(7, 20, 2000));
+        employees[3] = new BasePlusCommissionEmployee(300, 0.30, 5000, "Bob", "Johnson", "456-78-9012", new Date(12, 1, 1978));
+
+        // Calculate the payroll for each Employee and add a birthday bonus if applicable
+        for (Employee employee : employees) {
+            //polymorphism part
+            double earnings = employee.earnings();
+
+            // Check if the current month is the employee's birthday month
+            if (currentMonth == employee.getBirthDate().getMonth()) {
+                earnings += 100.0;
+                System.out.printf("%s%n%s: $%,.2f (with birthday bonus)%n%n",
+                                  employee, "earned", earnings);
+            } else {
+                System.out.printf("%s%n%s: $%,.2f%n%n",
+                                  employee, "earned", earnings);
+            }
+        }
+    }
+}
