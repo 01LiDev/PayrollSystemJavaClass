f0e8366e01ca83e8008fa4accef9a9e6459a1a5c
Johnson Li <liwindx15@gmail.com>
2023-04-25 00:44:02 -0400

    added myself as employee and a 600 and less condition to display

--- src/main/java/com/jldevelops/payrollsystem/PayrollSystem.java
+++ src/main/java/com/jldevelops/payrollsystem/PayrollSystem.java
@@ -8,14 +8,14 @@ public class PayrollSystem {
         int currentMonth = 3;
 
         // Create an array of Employee objects
-        Employee[] employees = new Employee[4];
+        Employee[] employees = new Employee[5];
 
         // Add sample employees to the array
         employees[0] = new SalariedEmployee("John", "Doe", "123-45-6789", 800.0, new Date(3, 15, 1990));
         employees[1] = new HourlyEmployee("Jane", "Smith", "234-56-7890", 20.0, 40, new Date(3, 10, 1985));
-        employees[2] = new CommissionEmployee("Alice", "Brown", "345-67-8901", 0.15, 0.06, new Date(7, 20, 2000));
+        employees[2] = new CommissionEmployee("Alice", "Brown", "345-67-8901", 0.15, 340, new Date(7, 20, 2000));
         employees[3] = new BasePlusCommissionEmployee(300, 0.30, 5000, "Bob", "Johnson", "456-78-9012", new Date(12, 1, 1978));
-
+        employees[4] = new SalariedEmployee("Johnson", "Li","232-234-2342", 1250.23, new Date(11,12,2001));
         // Calculate the payroll for each Employee and add a birthday bonus if applicable
         for (Employee employee : employees) {
             //polymorphism part
@@ -23,10 +23,14 @@ public class PayrollSystem {
 
             // Check if the current month is the employee's birthday month
             if (currentMonth == employee.getBirthDate().getMonth()) {
+                System.out.println("---Birthday Test Positive---");
                 earnings += 100.0;
                 System.out.printf("%s%n%s: $%,.2f (with birthday bonus)%n%n",
                                   employee, "earned", earnings);
-            } else {
+            }
+            //only show employees that have earnings that is 600 and less
+            else if(earnings <= 600){
+                System.out.println("---600 =< Test Positive---");
                 System.out.printf("%s%n%s: $%,.2f%n%n",
                                   employee, "earned", earnings);
             }
